3d9d74c5e3de5ce671000bb7d96f32e34908e4d7
adugbo aghogho <adugboaghogho@gmail.com>
2023-12-24 08:13:44 +0100

    frist commit

---
+++ main.js
@@ -0,0 +1,34 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+  });
+  
+  sr.reveal('.left-col, .products__container,  .footer__container, footer__info');
+  sr.reveal('.left-col, .contact__container', { delay: 600, origin: 'bottom'});
+  sr.reveal('.left-col, .brand__img, ', { interval: 100 });
+  sr.reveal('.primary-cta', { origin: 'right' });
+  sr.reveal('.Video-cta', { origin: 'left' });
